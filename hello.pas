dff767a72274dd61eb5f0bc5a6fe296024c0560f
Владислав Хохлов <vladkhokhlov220606@gmail.com>
2024-10-11 19:52:11 +0300

    коко джамбо

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿begin
+  println('доступны русский,  французский, английский языки');
+  var s:string;
+  println('на каком языке вы говорите?');
+  readln(s);
+  if (s='английский')or(s='Английский') then print('Hello')
+  else if (s='русский')or (s='Русский')then print('Привет')
+  else if (s='французский')or(s='Французский')then print('Bonjour')
+end.
